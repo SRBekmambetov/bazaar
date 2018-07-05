9655df36e4796b46b8d3b450c0feea74c3e2d2c7
SRBekmambetov <serg_bekm@mail.ru>
2018-07-05 11:28:48 +0300

    add basic structure

--- src/main/java/io/khasang/bazaar/controller/AppController.java
+++ src/main/java/io/khasang/bazaar/controller/AppController.java
@@ -1,5 +1,23 @@
 package io.khasang.bazaar.controller;
 
-public class AppController {
+import io.khasang.bazaar.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class AppController {
+    @Autowired
+    @Value("Barsik")
+    private Message message;
+
+    //http://localhost:8080/
+    @RequestMapping("/")
+    public String javaPageHello(Model model) {
+        model.addAttribute("name", message.getName());
+        //hello.jsp
+        return "hello";
+    }
 }
