a4a2d0817251fb238698704c9354a6b06a03d9ae
SRBekmambetov <serg_bekm@mail.ru>
2018-07-05 15:02:35 +0300

    add basic structure

---
+++ src/main/java/io/khasang/bazaar/model/CreateTable.java
@@ -0,0 +1,37 @@
+package io.khasang.bazaar.model;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+public class CreateTable {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public CreateTable() {
+    }
+
+    public CreateTable(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public String createStatus() {
+        try {
+            jdbcTemplate.execute("DROP TABLE IF EXISTS films");
+            jdbcTemplate.execute("CREATE TABLE films (\n" +
+                    "code CHAR(5) CONSTRAINT firstkey PRIMARY KEY, \n" +
+                    "title VARCHAR(255) NOT NULL, \n" +
+                    "id INTEGER NOT NULL);");
+            return "Table created";
+        } catch (Exception e) {
+            return "Table creation failed" + e;
+        }
+    }
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+}
---
+++ web/WEB-INF/views/table.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: cb_cb
+  Date: 05.07.2018
+  Time: 14:24
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<p>${status}</p>
+</body>
+</html>
